f8069f434679fa38644141d9b1724c26d9e06f4e
1Hira0 <aryanandaparida86@gmail.com>
2023-05-08 16:16:31 +0530

    manga info scrape code done
    Scraping manga info from site finished

--- src/backend/m-nato/m-nato.ts
+++ src/backend/m-nato/m-nato.ts
@@ -1,23 +1,51 @@
-export function Manganato(MangaPath:string) {
-    getManga(MangaPath)
+import { Manga } from "../mangaOut";
+import {RKEY} from "./env.ts"
+
+export async function Manganato(MangaPath:string) {
+    if (!(await isAvailable(MangaPath))) {console.log('Not found');return false}
+    console.log("Found");
+    return (await getManga(MangaPath))
 }
+
+async function isAvailable(MangaPath:string) {
+    const response = await fetch("https://raw.githubusercontent.com/MALSync/MAL-Sync-Backup/master/data/pages/MangaNato/_index.json")
+    const mangaList = await response.json()
+    if (mangaList.includes(MangaPath)) {
+        return true
+    } return false
+}
+
 async function getManga(id: string) {
-    const response = await fetch(`https://manganato.com/${id}`) 
-    console.log(response);
-}
+    const response = await fetch('https://http-cors-proxy.p.rapidapi.com/', {
+            method: 'POST',
+            headers: { 
+                'content-type': 'application/json',
+                Origin: 'www.example.com',
+                'X-Requested-With': 'www.example.com',
+                'X-RapidAPI-Key': RKEY,
+                'X-RapidAPI-Host': 'http-cors-proxy.p.rapidapi.com'
+            },
+            body: JSON.stringify({
+                url: `https://manganato.com/${id}`
+            })
+        });
+    	const result = await response.text();
+        const chaptersList = [...result.matchAll(/"chapter-name text-nowrap" href="(?<chapForward>.+?)" title=".+?">(?<chapName>.+?)<\/a>/gm)!]
+        const chapters = []
+        for(let i=0;i<chaptersList.length;i++) {
+            chapters.push({link:chaptersList[i][1], name:chaptersList[i][2]})
+        }
+        const manga :Manga = {
+            title:result.match(/"story-info-right">\n<h1>(?<title>.+?)<\/h1>/)!.groups!['title'],
+            icon_url:new URL (result.match(/"img-loading" src="(?<icon_url>.+?)" alt="/)!.groups!['icon_url']),
+            chapters:chapters
+        }
+        return manga
+} 
 
-function getChapters(manga:URL, num_chapters:number) {
-    const chapters = [];
-    for (let chapter= 0;chapter<num_chapters;chapter++) {
-        getChapter(manga, chapter)
+
+async function getChapters(chapters:Manga['chapters']) {
+    for (let i = 0; i < chapters.length; i++) {
+        await fetch(chapters[i].link) //to be completed and used in above function
     }
-}
-
-function getChapter(manga:URL, chapter:number) {
-    const response = fetch(manga+`chapter-${chapter}`)
-}
-
-function manganato_ChapterLinks(id:string) {
-    return ["ch1", "ch2", "ch3", "ch4"] 
-
 }
